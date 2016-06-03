3f74e981a26952352c187d268e4ef3254310033a
nashera <zhangxi1014@gmail.com>
2016-06-03 21:39:45 +0800

    update

--- Jun03/bubbleSort.c
+++ Jun03/bubbleSort.c
@@ -1,5 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
+void swap(int *a, int *b){
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
 void bubbleSort(int *a, int length){
+	for (int i = 0; i < length; ++i)
+	{
+		for (int j = i + 1; j < length; ++j)
+		{
+			if(*(a + i) > *(a + j)){
+				swap(a + i, a + j);
+			}
+		}
+	}
 
+}
+
+int main(int argc, char const *argv[])
+{
+	int a[] = {2, 1, 3, 5, 3, 4, 8, 1, 2, 23, 7};
+	bubbleSort(a, sizeof(a) / sizeof(int));
+	for (int i = 0; i < sizeof(a) / sizeof(int); ++i)
+	{
+		printf("%d\t", *(a + i));
+	}
+	printf("\n");
+	return 0;
 }
---
+++ Jun03/insertSort_test02.c
@@ -0,0 +1,5 @@
+#include <stdlib.h>
+#include <stdio.h>
+void insertSort(int *a, int length){
+
+}
